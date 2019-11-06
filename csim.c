deef61891279aec0598bff4a0e7ba6711c727db0
Timothée Darcet <timothee.darcet+git@m4x.org>
2019-11-06 18:28:56 +0100

    Implemented cache sim

--- csim.c
+++ csim.c
@@ -1,3 +1,7 @@
+/* INF559 - cachelab
+ * Project by Timothée Darcet timothee.darcet
+ */
+
 #include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,13 +9,26 @@
 
 #include "cachelab.h"
 
+#define WRITE_ALLOCATE 1 //unused for now, supposition is that we do write-allocate
 
 /* Globals set by command line args */
 int verbosity = 0; /* print trace if set */
 int s = 0;         /* set index bits */
 int b = 0;         /* block offset bits */
 int E = 0;         /* associativity */
-char* trace_file = NULL;
+int t = 0;         /* tag length */
+int S = 0;         /* number of sets */
+int L = 0;         /* line size */
+char *trace_file = NULL;
+char *cache = NULL;
+int *lastuse = NULL;
+int timer = 0;
+
+typedef struct line
+{
+    unsigned valid:1;
+    unsigned int rest;
+} line;
 
 /*
  * printUsage - Print usage info
@@ -31,6 +48,93 @@ void printUsage(char* argv[])
     printf("  linux>  %s -v -s 8 -E 2 -b 4 -t traces/yi.trace\n", argv[0]);
     exit(0);
 }
+
+int *parse(int *counts) {
+  FILE *fid;
+  if ((fid = fopen(trace_file, "r")) == NULL)
+  {
+    while (!feof(fid))
+      {
+        char operation;
+        unsigned int address;
+        if (fscanf(fid, " %c %x,%d", &operation, &address, NULL))
+        {
+          int ret;
+          switch(operation) {
+            case 'L':
+              ret = load(address);
+              break;
+            case 'S':
+              ret = store(address);
+              break;
+            case 'M':
+              ret = modify(address);
+              break;
+            default:
+              fprintf(stderr, "Invalid operation: %c", operation);
+              ret = 0;
+              break;
+          }
+          counts[0] += ret & 3;
+          counts[1] += (ret >> 2) & 3;
+          counts[2] += (ret >> 4) & 3;
+        }
+      }
+  }
+}
+
+int load(unsigned int address) {
+  // Try to find entry in cache
+  unsigned int tag = address >> (32 - t);
+  int sidx = (address << t) >> (32 - s);
+  for (int lidx = 0; lidx < E; lidx++) {
+    line *l = (struct line *) (cache + sidx * E * L + lidx * L);
+    int currentTag = l->rest >> (32 - t);
+    if (l->valid && (currentTag == tag)) {
+      lastuse[sidx * E + lidx] = timer;
+      timer++;
+      return 1; // hit
+    }
+  }
+  // Entry not found
+  // Try to replace an unused line
+  for (int lidx = 0; lidx < E; lidx++) {
+    line *l = (struct line *) (cache + sidx * E * L + lidx * L);
+    if (!l->valid) {
+      l->valid = 1;
+      l->rest = tag << (32 - t);
+      lastuse[sidx * E + lidx] = timer;
+      timer++;
+      return 4; // miss
+    }
+  }
+  // No unused line found
+  // Find LRU line to evict
+  int lru = 0;
+  int lru_timestamp = timer;
+  for (int lidx = 0; lidx < E; lidx++) {
+    int ts = lastuse[sidx * E + lidx];
+    if (ts < lru_timestamp) {
+      lru_timestamp = ts;
+      lru = lidx;
+    }
+  }
+  line *l = (struct line *) (cache + sidx * E * L + lru * L);
+  l->valid = 1;
+  l->rest = tag << (32 - t);
+  lastuse[sidx * E + lru] = timer;
+  timer++;
+  return 20; // miss, evict
+}
+
+int store(unsigned int address) {
+  return load(address);
+}
+
+int modify(unsigned int address) {
+  return load(address) + store(address);
+}
+
 
 /*
  * main - Main routine 
@@ -72,6 +176,15 @@ int main(int argc, char* argv[])
     exit(1);
   }
 
-  printSummary(0, 0, 0);
+  t = 32 - b - s;
+  S = (int)pow(2, s);
+  L = (1 + t + (int)pow(2, b));
+  cache = calloc(S, E * L);
+  lastuse = calloc(S * E, sizeof(int));
+  int counts[3] = {0, 0, 0};
+  parse(counts);
+  free(cache);
+  free(lastuse);
+  printSummary(counts[0], counts[1], counts[2]);
   return 0;
 }
